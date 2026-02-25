ca9403675d15b044d788f41c73ca79213631fe4a
NiNoX14 <hardstuckd5lul@gmail.com>
2026-02-25 18:42:52 +0200

    feat: check if .env exists

--- apps/back-end/src/db/schema/config.schema.ts
+++ apps/back-end/src/db/schema/config.schema.ts
@@ -1,6 +1,6 @@
-import { integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { integer, serial, pgTable } from 'drizzle-orm/pg-core';
 
 export const config = pgTable('config', {
-  id: text('id').primaryKey(),
+  id: serial('id').primaryKey(),
   base_tax: integer().notNull(),
 });
--- apps/back-end/src/utils/index.ts
+++ apps/back-end/src/utils/index.ts
@@ -1,7 +1,30 @@
 import path from 'node:path';
-
+import fs from 'node:fs';
 export function getConfigPath() {
-  return process.env.NODE_ENV === 'prod'
-    ? path.resolve(process.cwd(), '..', 'config', '.env.production.local')
-    : path.resolve(process.cwd(), 'config', '.env.development.local');
+  let configPath: string;
+  if (process.env.NODE_ENV === 'prod') {
+    configPath = path.resolve(
+      process.cwd(),
+      '..',
+      'config',
+      '.env.production.local',
+    );
+  } else {
+    configPath = path.resolve(
+      process.cwd(),
+      'config',
+      '.env.development.local',
+    );
+  }
+  if (fs.existsSync(configPath)) {
+    return configPath;
+  }
+
+  configPath = path.resolve(process.cwd(), '.env');
+
+  if (fs.existsSync(configPath)) {
+    return configPath;
+  } else {
+    throw Error('Provide env config ');
+  }
 }
