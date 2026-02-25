3c06e24e3b68c46755abc02970a4c94cc347d2e0
NiNoX14 <hardstuckd5lul@gmail.com>
2026-02-25 03:47:16 +0200

    wip: orders page

--- apps/front-end/app/dashboard/orders/page.tsx
+++ apps/front-end/app/dashboard/orders/page.tsx
@@ -1,3 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
+
 export default function DashboardOrders() {
-  return <h1 className="text-2xl font-bold">Orders Page</h1>;
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center font-sans dark:bg-black">
+      <div className="flex min-h-screen p-6 w-full max-w-4xl flex-col items-center bg-white dark:bg-black sm:items-start">
+        <Card className="w-full border-none shadow-none">
+          <CardHeader className="flex items-center justify-between">
+            <CardTitle className="font-bold text-4xl capitalize">
+              Orders list
+            </CardTitle>
+            <Button>
+              <Plus /> Create order
+            </Button>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-6">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-lg">Filters</CardTitle>
+              </CardHeader>
+              <CardContent className="px-0">{/* TODO:  filters */}</CardContent>
+            </Card>
+            <div className="w-full"> {/* TODO: orders table  */}</div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
